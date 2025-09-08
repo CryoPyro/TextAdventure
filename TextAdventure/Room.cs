c0d3525d82d09bfe47d2e13cbcaa7bed652d8a8e
CryoPyro <viggowildberry@gmail.com>
2025-09-08 17:11:16 +0200

    Added; * Room, a place that stores events that happen when entered or when the player chooses an action * Moving between rooms (maybe refactor to TravelEvent class) * AEvent and subclasses, for events that happen in rooms * Attack, does damage to entities * Enemy, attacks an entity with a random attack

---
+++ TextAdventure/Room.cs
@@ -0,0 +1,54 @@
+﻿namespace TextAdventure;
+
+
+public class Room(string name, int[] connections)
+{
+    public string Name = name;
+    public int[] Connections = connections;
+
+    public AEvent? OnEnter;
+    public AEvent[] Actions;
+}
+
+
+public abstract class AEvent
+{
+    public string Description;
+}
+
+
+public class ShopEvent : AEvent;
+
+public class FightEvent : AEvent
+{
+    public Enemy[] enemies;
+}
+
+public class Attack
+{
+    public string Name;
+    public string Description;
+    public int Damage;
+}
+
+public class Entity
+{
+    public string Name;
+    public int Health;
+    public Attack?[] Attacks = new Attack[4];
+}
+
+public class Enemy : Entity
+{
+    public void Attack(Entity entity)
+    {
+        var possibleAttacks = Attacks.Where(a => a != null).ToArray();
+        var random = new Random();
+        var attack = possibleAttacks[random.Next(0, possibleAttacks.Length)];
+    }
+}
+
+public class Player : Entity
+{
+    
+}
